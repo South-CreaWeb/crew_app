12d68ca0e9754c143a2aa0802d090e8cce2b59ab
South-CreaWeb <south-creaweb@outlook.fr>
2026-05-02 11:37:40 +0200

    Connecting Front <-> Back

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import type { CrewData } from './types/crew.type'
+
+const API_URL: string = "http://localhost:3000/crew"
+
+const crews = ref<CrewData[]>([])
+
+onMounted( async () =>  {
+  const data = await fetch(API_URL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  crews.value = await data.json()
+})
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>List Profil crews</h1>
+  <ul>
+    <li v-for="crew in crews" :key="crew.customId">
+      {{ crew.name }}--{{ crew.role }}
+    </li>
+  </ul>
 </template>
 
-<style scoped></style>
---
+++ frontend/src/types/crew.type.ts
@@ -0,0 +1,7 @@
+export type CrewData = {
+    customId: string
+    name: string,
+    role: string,
+    available: true,
+    note: string
+}
